16ebac86f54f028ef18413d95b3891de2596c23f
kplvashishat <kapil.vashishat@yahoo.com>
2018-09-30 21:57:56 +0530

    Two Components added Home & Header

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,30 @@
-console.log("React JS .. Hey Kapil");
+// console.log("React JS .. Hey Kapil");
+import React from "react";
+import { render } from "react-dom";
+
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
+
+class App extends React.Component
+{
+    render()
+    {
+        return(
+            <div className = "container">
+                <h1>Hello Kapil</h1>
+                <div className="row">
+                    <div className="col-xs-10">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10">
+                        <Home/>
+                    </div>
+                </div>  
+            </div>
+        );
+    }
+}
+
+render(<App/> , window.document.getElementById("app"));
